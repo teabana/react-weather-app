c8c915c86f05e6396fefc90c42734869f0ab6925
teabana <micky59322@gmail.com>
2022-07-11 22:00:52 +0900

    add_form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import Title from "./components/Title";
+import Form from "./components/Form";
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <h1>こんにちは</h1>
+      <Title />
+      <Form />
     </div>
   );
 }
