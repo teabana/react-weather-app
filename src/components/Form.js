c918bd9581235adcb150953bff26818b6fd53523
teabana <micky59322@gmail.com>
2022-07-12 08:57:06 +0900

    form_lastedit

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,19 @@
 import { useState } from "react";
+import axios from "axios";
+
 
 
 const Form = () => {
   const [city, setCity] = useState("");
+  const getWether = (e) => {
+    e.preventDefault();
+    axios.get("http://api.weatherapi.com/v1/current.json?key=0cc30297d28c4a8c919133127221107&q=London&aqi=no")
+      .then(res => console.log(res))
+  };
   return (
     <form>
       <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)}/>
-      {city}
-      <button type="submit">Get Weather</button>
+      <button type="submit" onClick={getWether}>Get Weather</button>
     </form>
   );
 };
